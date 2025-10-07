c711a1a4e04500c6da2ec323d1727cf14ba3328f
MigzReyes <johnmigzreyes0@gmail.com>
2025-10-07 16:52:57 +0800

    The initial commit of OneSTI proposed Web-Based Online Tuition Payment

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace oneSTIOnlineTuitionPayment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
