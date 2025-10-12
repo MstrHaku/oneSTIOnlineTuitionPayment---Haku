8f113147924a6ddf036d84aea5a41041f5821852
Haku <mstrhaku231@gmail.com>
2025-10-12 20:34:42 +0800

    fix: Fixed .sln and .csproj files

--- Controllers/GcashController.cs
+++ Controllers/GcashController.cs
@@ -1,18 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace oneSTIOnlineTuitionPayment_Sai.Controllers
+namespace oneSTIOnlineTuitionPayment.Controllers
 {
     public class GcashController : Controller
     {
+        private readonly ILogger<GcashController> _logger;
+
+        public GcashController(ILogger<GcashController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult PhoneNumber()
         {
             return View("Gcash-PhoneNumber");
         }
 
-public IActionResult OtpPage()
-{
-    return View("Gcash-OtpPage");
-}
-
+        public IActionResult OtpPage()
+        {
+            return View("Gcash-OtpPage");
+        }
     }
 }
+
+
